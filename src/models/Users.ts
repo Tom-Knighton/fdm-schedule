d7cb28485b83a1e228e1dee98ab07d7045f297fa
Tom Knighton <tomknighton@icloud.com>
2022-04-05 14:21:37 +0100

    add profilePicUrl to users

--- src/models/Users.ts
+++ src/models/Users.ts
@@ -1,18 +1,18 @@
 import Module from "module";
 
 export abstract class User {
-    constructor(public name: String, public username: String, public password: String, public location: String, public timezone: String) {}
+    constructor(public name: String, public username: String, public password: String, profilePicUrl: String, public location: String, public timezone: String) {}
 };
 
 export class Scheduler extends User {
-    constructor(name: String, username: String, password: String, location: String, timeZone: String, public schedulerId: String, public officeNumber: String) {
-        super(name, username, password, location, timeZone);
+    constructor(name: String, username: String, password: String, profilePicUrl: String, location: String, timeZone: String, public schedulerId: String, public officeNumber: String) {
+        super(name, username, password, profilePicUrl, location, timeZone);
     }
 }
 
 export class Trainer extends User {
-    constructor(name: String, username: String, password: String, location: String, timeZone: String, public trainerId: String, public phoneNumber: String, public skills: String[], public modules: Module[], public availability: Date[][], public remoteTeaching: boolean) {
-        super(name, username, password, location, timeZone);
+    constructor(name: String, username: String, password: String, profilePicUrl: String, location: String, timeZone: String, public trainerId: String, public phoneNumber: String, public skills: String[], public modules: Module[], public availability: Date[][], public remoteTeaching: boolean) {
+        super(name, username, password, profilePicUrl, location, timeZone);
     }
 
     AddSkill(skillname: String) {
@@ -25,8 +25,8 @@ export class Trainer extends User {
 }
 
 export class Admin extends User {
-    constructor(name: String, username: String, password: String, location: String, timeZone: String, public adminId: String) {
-        super(name, username, password, location, timeZone);
+    constructor(name: String, username: String, password: String, profilePicUrl: String, location: String, timeZone: String, public adminId: String) {
+        super(name, username, password, profilePicUrl, location, timeZone);
     }
 }
 
