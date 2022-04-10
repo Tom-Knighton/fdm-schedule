fb14aa0bee5e821c4a1e742b050b8c0aaf58bff4
IFMW01 <55697240+IFMW01@users.noreply.github.com>
2022-04-10 12:07:27 +0100

    Update AccountService.ts
    Update postcode format to be verified by form , added the loading of users account info on form.

--- src/lib/AccountService.ts
+++ src/lib/AccountService.ts
@@ -1,3 +1,5 @@
+import { getActiveElement } from "@testing-library/user-event/dist/utils";
+import { json } from "stream/consumers";
 import {Account} from "../models/Account";
 export const Accounts: Array<Account> = [
     new Account(
@@ -9,7 +11,7 @@ export const Accounts: Array<Account> = [
       "7 Maple rd",
       "London",
       "London",
-      "E6 7GD"
+      "E14NS"
     ),
     new Account(
         "JohnJohn",
@@ -20,7 +22,7 @@ export const Accounts: Array<Account> = [
         "2 Tea lane",
         "London",
         "London",
-        "E2 2GD"
+        "E14NS"
     ),
     new Account(
         "JohnJohnJohn",
@@ -31,9 +33,10 @@ export const Accounts: Array<Account> = [
         "3 Trifecta lane",
         "London",
         "London",
-        "E3 3KD"
+        "E14NS"
     ),
     new Account(
+        
         "Mr Mustafa",
         "Bozkurt",
         "m.bozkurt@fdmgroup.com",
@@ -42,25 +45,42 @@ export const Accounts: Array<Account> = [
         "2 Redbridge Road",
         "London",
         "London",
-        "E8 IG5"
+        "E14NS"
     ),
 ];
 class AccountService{
     static getAccountbyEmail(email: String){
-        const validAccount = Accounts.filter((u) => u.email === email);
-        return validAccount.length > 0 ? validAccount[0] : null;
+        const email2 = email
+        const account = localStorage.getItem("account." + email2);
+        if (account === null){
+            const validAccount = Accounts.filter((u) => u.email === email);
+            if (validAccount.length >0){
+                localStorage.setItem('account.'+(validAccount[0].email), JSON.stringify(validAccount[0]));
+                return validAccount[0];
+            }else{
+                return null;
+            }
+        }else{
+            const localAccount = JSON.parse(account);
+            const newAccount = new Account(
+             localAccount.name, localAccount.surname, localAccount.email,localAccount.personalPhone,localAccount.workPhone,localAccount.street,localAccount.city,localAccount.county,localAccount.postcode
+            );
+            return newAccount;
+
+        }
+            
     }
 
-    static updateAccountInfo(account: Account, name: String, surname: String, email: String, personalPhone: String, workPhone: String, street: string, city: string, county: string, postcode: string){
+    static updateAccountInfo(account: Account, name: String, surname: String, personalPhone: String, workPhone: String, street: string, city: string, county: string, postcode: string){
         account.name = name
         account.surname = surname
-        account.email = email
         account.personalPhone = personalPhone
         account.workPhone = workPhone
         account.street = street
         account.city = city
         account.county = county
         account.postcode = postcode
+        localStorage.setItem('account.'+(account.email), JSON.stringify(account))
     }
 }
 
