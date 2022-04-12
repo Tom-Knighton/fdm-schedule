de3ee963c2f75b7775928c13637a2538bb78929b
remizla <lukas7b@hotmail.com>
2022-04-12 20:12:01 +0100

    provisionally completed form, added basic function run upon clicking of submit button

--- src/components/assignModule.js
+++ src/components/assignModule.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import "../pages/styleform.css";
 
 // main role: scheduler adds skills to trainer
-// loops through list of users and loads trainer users into drop down.
+// loops through list of users and loads trainer users into drop down. (using harcoded users)
 // manually add in tasks
 
 /* 
@@ -16,19 +16,40 @@ if checks fail, bolden the labels, make them red and change text as required
 
 */
 
+/*
+  psuedocode for adding module (on running code with button):
+
+  if module drop down or user drop down == "select...":
+    make text red and embolden (or change h5 text and make it red)
+  else:
+    loop through the code
+*/
+
 export default function assignModule() {
+  function assignModule() {
+    alert("Test!");
+  }
+
   return (
     <div>
       <form id="assignUserSelect">
         <h1>Assign trainer to module</h1>
         <h5>Select a trainer and the module you want to assign to them.</h5>
 
-        <div id="assignTrainerSelect"></div>
+        <div id="assignTrainerSelect">
+          <label for="users">Select a trainer:</label>
+          <select name="modules">
+            <option value="User 1">User 1</option>
+            <option value="User 2">User 2</option>
+            <option value="User 3">User 3</option>
+            <option value="Mustafa Bozkurt">Mustafa Bozkurt</option>
+          </select>
+        </div>
 
         <div id="assignModuleSelect">
           <label for="modules">Select a module:</label>
           <select name="modules" id="modules">
-            <option value="selectOption">Select module</option>
+            <option value="selectOption">Select...</option>
             <optgroup label="Professional">
               <option value="Microsoft Office">Microsoft Office</option>
               <option value="Communication">Communication</option>
@@ -47,7 +68,9 @@ export default function assignModule() {
           </select>
         </div>
 
-        <div id="assignButtons"></div>
+        <div id="assignButtons">
+          <Link to={assignModule}>Add module</Link>
+        </div>
       </form>
     </div>
   );
